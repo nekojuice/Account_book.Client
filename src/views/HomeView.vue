7fbc638f493e7a65946460428c3eead7406bcf0e
Jose Wu <josewu@uictech.com.tw>
2024-05-22 18:09:11 +0800

    feat: 製作簡易登入畫面

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,26 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const account = ref('')
+const password = ref('')
+function login() {
+  console.log(account.value)
+  console.log(password.value)
+}
+</script>
 
 <template>
-  <main></main>
+  <main>
+    <div class="container">
+      <form @submit.prevent="onSubmit">
+        <div class="input-group mb-3 d-flex flex-column">
+          <span>帳號</span>
+          <input type="text" class="mb-3" v-model="account" />
+          <span>密碼</span>
+          <input type="password" class="mb-3" v-model="password" />
+          <button type="submit" class="btn btn-primary" @click="login">登入</button>
+        </div>
+      </form>
+    </div>
+  </main>
 </template>
