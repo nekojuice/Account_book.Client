95c3cd28743e8efa51b6d6b8e2529f5f9d06a3e7
Jose Wu <josewu@uictech.com.tw>
2024-05-23 16:21:59 +0800

    feat: get帳本資料顯示於表格

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,15 +4,16 @@ import { authService } from '@/services/authService'
 import { accountingService } from '@/services/accountingService'
 
 // 登入資訊物件
-const userLoginInfo = ref({ email: '', password: '' })
+const userLoginInfo = ref({ email: '111', password: '222' })
 // 登入
-async function login() {
+async function onLogin() {
   await authService.login(userLoginInfo.value)
+  const result = await accountingService.getAllAccountingData()
+  accountingData.value = result.returnData
 }
 
-function testbtn() {
-  console.log(accountingService.getAllAccountingData())
-}
+// 帳本資料
+const accountingData = ref([])
 </script>
 
 <template>
@@ -22,14 +23,38 @@ function testbtn() {
         class="sm input-group d-flex flex-column bg-light border border-3 rounded-3 m-auto p-3 maxLoginFrame"
       >
         <span>帳號</span>
-        <input type="text" class="mb-3" v-model="userLoginInfo.email" />
+        <input type="text" class="mb-3" v-model="userLoginInfo.email" @keyup.enter="onLogin" />
         <span>密碼</span>
-        <input type="password" class="mb-3" v-model="userLoginInfo.password" />
-        <button type="button" class="btn btn-primary" @click="login">登入</button>
+        <input
+          type="password"
+          class="mb-3"
+          v-model="userLoginInfo.password"
+          @keyup.enter="onLogin"
+        />
+        <button type="button" class="btn btn-primary" @click="onLogin" @keyup.enter="onLogin">
+          登入
+        </button>
       </div>
     </div>
 
-    <div><button type="button" @click="testbtn">test</button></div>
+    <table class="table">
+      <thead>
+        <tr>
+          <td>消費日期</td>
+          <td>類型</td>
+          <td>金額</td>
+          <td>備註</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="data in accountingData" :key="data.accountingId">
+          <td>{{ data.recordTime }}</td>
+          <td>{{ data.typeName }}</td>
+          <td>{{ data.money }}</td>
+          <td>{{ data.message }}</td>
+        </tr>
+      </tbody>
+    </table>
   </main>
 </template>
 
